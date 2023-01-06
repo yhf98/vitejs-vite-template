b6583e273473f4d7995945e92537d517c0bf941e
yhf98 <46492705+yhf98@users.noreply.github.com>
2023-01-06 11:29:28 +0800

    5 files modified, 2 added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import { useCounterStore } from './stores/counter.ts';
+
+const store = useCounterStore();
 </script>
 
 <template>
+  <HelloWorld msg="Pinia" />
+  <h1>{{ store.count }}</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="store.increment">增加</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
